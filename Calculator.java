8ca84650eb8e7f13d14a14a29c7823e42c1930d3
Your Name <you@example.com>
2026-02-17 12:47:12 +0400

    Add sum and Difference

--- Calculator.java
+++ Calculator.java
@@ -7,9 +7,22 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
 
 
+        System.out.println("Enter first number : ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2 ;
+        int difference =  num1 - num2;
+
+        System.out.println("Sum "+ sum);
+        System.out.println("Difference " + difference);
 
 
+
+        System.out.println("Thats all... ");
         scanner.close();
+
 
     }
 }
